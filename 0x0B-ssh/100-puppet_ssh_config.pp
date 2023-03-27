75b8bc8402e0b465868c300eec043446f1ab8654
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-27 18:56:52 +0100

    client config w/ puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
 # setting up client config
 include stdlib
 
-file_line { "Turn off Password Auth":
+file_line { 'Turn off Password Auth':
     ensure  => present,
-    path    => "etc/ssh/ssh_config",
-    line    => "    PasswordAuthentication no",
+    path    => 'etc/ssh/ssh_config',
+    line    => '    PasswordAuthentication no',
     replace => true,
 }
 
-file_line { "Declare Identity File":
+file_line { 'Declare Identity File':
     ensure  => present,
-    content => "etc/ssh/ssh_config",
-    line    => "    IdentityFile ~/.ssh/school",
+    content => 'etc/ssh/ssh_config',
+    line    => '    IdentityFile ~/.ssh/school',
     replace => true,
 }
