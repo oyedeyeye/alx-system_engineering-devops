f55b847ac9b550bb87ccc5b3f12d22a4e1bd167e
Hezekiah Oyemade <oyedey@gmail.com>
2023-03-28 18:17:46 +0100

    update to advanced task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@ include stdlib
 
 file_line { 'Turn off Password Auth':
     ensure  => present,
-    path    => '~/.ssh/config',
+    path    => 'etc/ssh/ssh_config',
     line    => '    PasswordAuthentication no',
     replace => true,
 }
 
 file_line { 'Declare Identity File':
     ensure  => present,
-    content => '~/.ssh/config',
+    content => 'etc/ssh/ssh_config',
     line    => '    IdentityFile ~/.ssh/school',
     replace => true,
 }
