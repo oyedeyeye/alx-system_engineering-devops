5ac8bf375ace9313b2379f8e183a63d8c00aaa1c
Hezekiah Oyemade <oyedey@gmail.com>
2023-04-13 16:26:57 +0100

    task 2 updated

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,27 +2,12 @@
 # The name of the custom HTTP header must be X-Served-By
 # Value of the custom HTTP header must be hostname of server Nginx is on
 
-exec { 'server update':
-    command  => 'apt-get update',
+exec { 'command':
+    command  => 'apt-get update -y;
+    apt-get install nginx -y;
+    sudo sed -i "/listen [::]:80 default_server;/a add_header X-Served-By $hostname" /etc/nginx/sites-available/default;
+    service nginx restart',
     user     => 'root',
     provider => 'shell',
 }
 
-package { 'nginx':
-    ensure   => 'installed',
-    provider => 'apt',
-    require  => Exec['server update'],
-}
-
-file_line { 'custom_http_header':
-    path     => '/etc/nginx/sites-available/default',
-    before   => 'root /var/www/html;',
-    line     => 'add_header X-Served-By "$hostname";',
-    notify   => Service['nginx running'],
-    require  => Package['nginx'],
-}
-
-service { 'nginx running':
-    ensure   => 'running',
-    require  => Package['nginx'],
-}
