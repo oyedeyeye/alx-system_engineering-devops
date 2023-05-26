cfae8cdffaf16f0558d0d7ecdf7ff697263653ff
Hezekiah Oyemade <oyedey@gmail.com>
2023-05-26 23:55:57 +0100

    request response to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that, using REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    u_id = "{}".format(sys.argv[1])
+    user_url = "https://jsonplaceholder.typicode.com/users/"
+    todo_url = "https://jsonplaceholder.typicode.com/todos/"
+    user = requests.get(user_url + u_id).json()
+    todos = requests.get(todo_url, params={"userId": u_id}).json()
+
+    completed_task = [task.get("title") for task in todos
+                      if task.get("completed") is True]
+    print("Employee {:s} is done with tasks({:d}/{:d}):".format(
+            user.get('name'), len(completed_task), len(todos)))
+    result = ["\t {}".format(title) for title in completed_task]
+    [print(item.expandtabs(4)) for item in result]
